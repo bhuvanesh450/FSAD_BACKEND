e71c8be45466ddad28091bfb13928649557cb5ff
BHUVANESH <2400032064@kluniversity.in>
2026-04-28 12:58:09 +0530

    Add package declaration to FsadBackendApplication

--- src/main/java/com/klu/FsadBackendApplication.java
+++ src/main/java/com/klu/FsadBackendApplication.java
@@ -1,3 +1,6 @@
+package com.klu;
+
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
