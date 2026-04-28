915a1fd10afa86f0f7edbbc651c0e6f69fee92e0
BHUVANESH <2400032064@kluniversity.in>
2026-04-28 13:05:24 +0530

    Update FsadBackendApplication.java

--- src/main/java/com/klu/FsadBackendApplication.java
+++ src/main/java/com/klu/FsadBackendApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 
 @SpringBootApplication(
     exclude = {
         DataSourceAutoConfiguration.class,
-        HibernateJpaAutoConfiguration.class
+        HibernateJpaAutoConfiguration.class,
+        JpaRepositoriesAutoConfiguration.class
     }
 )
 public class FsadBackendApplication {
