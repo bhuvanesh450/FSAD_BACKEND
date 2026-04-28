6806a32ecc1eeaca7f0eaa47a48989bf101c59f0
BHUVANESH <2400032064@kluniversity.in>
2026-04-28 12:56:23 +0530

    Exclude DataSourceAutoConfiguration in Spring Boot app

--- src/main/java/com/klu/FsadBackendApplication.java
+++ src/main/java/com/klu/FsadBackendApplication.java
@@ -1,9 +1,7 @@
-package com.klu;
-
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class FsadBackendApplication {
 
     public static void main(String[] args) {
